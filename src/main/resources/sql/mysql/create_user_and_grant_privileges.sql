fb4170668875ae0edb2a2ff190e4ffa5607b3c74
xmx0632 <xmx0632@gmail.com>
2013-10-24 15:29:36 +0800

    bugfix:创建mysql用户

--- src/main/resources/sql/mysql/create_user_and_grant_privileges.sql
+++ src/main/resources/sql/mysql/create_user_and_grant_privileges.sql
@@ -1,6 +1,6 @@
 use mysql;
 DELETE FROM mysql.user WHERE User="management";
-insert into mysql.user(Host,User,Password) values("localhost","management",password("management"));
+insert into mysql.user(Host,User,Password,ssl_cipher,x509_issuer,x509_subject) values("localhost","management",password("management"),'','','');
 grant ALL PRIVILEGES ON management.* TO 'management'@'localhost' IDENTIFIED BY 'management' WITH GRANT OPTION;
 flush privileges;
 commit;
