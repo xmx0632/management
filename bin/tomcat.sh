c81fe6545095e8259b99b0487ab9b9f69bbe92b3
xmx0632 <xmx0632@gmail.com>
2014-01-05 18:00:37 +0800

    tomcat启动脚本修改

--- bin/tomcat.sh
+++ bin/tomcat.sh
@@ -2,6 +2,6 @@
 echo [INFO] Use maven tomcat-plugin run the project.
 cd ..
 
-export MAVEN_OPTS=%MAVEN_OPTS% -XX:MaxPermSize=128m
+#export MAVEN_OPTS=%MAVEN_OPTS% -XX:MaxPermSize=128m
 mvn tomcat:run -Dmaven.tomcat.port=8080
 cd bin
