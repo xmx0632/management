d4ed4cea9ba89621153999906a78416a51c21c8a
xmx0632 <xmx0632@gmail.com>
2014-05-14 21:09:49 +0800

    建用户脚本改为标准sql

--- src/main/resources/sql/mysql/create_user_and_grant_privileges.sql
+++ src/main/resources/sql/mysql/create_user_and_grant_privileges.sql
@@ -1,6 +1,8 @@
 use mysql;
 DELETE FROM mysql.user WHERE User="management";
-insert into mysql.user(Host,User,Password,ssl_cipher,x509_issuer,x509_subject) values("localhost","management",password("management"),'','','');
+flush privileges;
+--insert into mysql.user(Host,User,Password,ssl_cipher,x509_issuer,x509_subject) values("localhost","management",password("management"),'','','');
+CREATE USER management IDENTIFIED BY 'management';
 grant ALL PRIVILEGES ON management.* TO 'management'@'localhost' IDENTIFIED BY 'management' WITH GRANT OPTION;
 flush privileges;
 commit;
